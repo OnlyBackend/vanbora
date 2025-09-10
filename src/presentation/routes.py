673a9f2968dfad2502ef251bc086996c6dd66f14
Jonnison Lima <jonnison.1234@gmail.com>
2025-09-09 23:14:06 -0300

    health check endpoint

--- src/presentation/routes.py
+++ src/presentation/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from src.presentation import auth, users, trips, reservations
+from src.presentation import auth, users, trips, reservations, health_check
 
 router = APIRouter()
 
+router.include_router(health_check.router, prefix="/api", tags=["health"])
 router.include_router(auth.router, prefix="/api", tags=["auth"])
 router.include_router(users.router, prefix="/api", tags=["users"])
 router.include_router(trips.router, prefix="/api", tags=["trips"])
